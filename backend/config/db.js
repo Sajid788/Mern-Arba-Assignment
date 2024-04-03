d195a90bba66d71afa6f792a9f5105be676b21cd
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-03 21:00:01 +0530

    mongodb connected

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const connection = async () => {
+  try {
+    // Attempt to connect to MongoDB using the provided URL from the environment variables
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    // If an error occurs during the connection, log the error message
+    console.log(error);
+  }
+};
+
+module.exports = { connection };
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+
+const { connection } = require("./config/db");
+
+const app = express();
+app.use(express.json());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send({msg:"Welcome to the home page!"});
+});
+
+
+// Not found route
+app.use((req, res) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection();
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`Server is running at ${process.env.PORT}`);
+});
